77efb1dba3d3d683654c1a15afe74a5a89407bd2
HaoTNN <HaoTNN@gmail.com>
2013-12-09 01:32:46 -0800

    - Fix: vector3.h
    - Add: halfedge.h, main.cpp
    
    - halfedge structure is implemented, need to input a sample mesh file and
      draw using GLUT

--- vector3.h
+++ vector3.h
@@ -7,45 +7,46 @@
 #define VECTOR3_H
 
 #include <math.h>
+#include <iostream>
 
 struct Vector3{
 	float x, y, z;
 	
 	Vector3()
-		: x(0.0), y(0.0), z(0)
+		: x(0.0), y(0.0), z(0.0)
 	{}
 	
 	Vector3( float i, float j, float k )
 		: x(i), y(j), z(k)
 	{}
 	
-	Vector3( Vector3 v )
+	Vector3( const Vector3 &v )
 		: x(v.x), y(v.y), z(v.z)
 	{}
 	
-	Vector3 operator+( const Vector3 &rhs ){
+	Vector3 operator+( const Vector3 &rhs ) const{
 		return Vector3( x+rhs.x, y+rhs.y, z+rhs.z );
 	}
-	Vector3 operator-( const Vector3 &rhs ){
+	Vector3 operator-( const Vector3 &rhs ) const{
 		return Vector3( x-rhs.x, y-rhs.y, z-rhs.z );
 	}
-	Vector3 operator*( const Vector3 &rhs ){
-		return Vector3( x*rhs.x, y*rhs.y, z*rhs.z );
+	Vector3 operator*( float rhs ){
+		return Vector3( x*rhs, y*rhs, z*rhs );
 	}
-	Vector3 operator/( const Vector3 &rhs ){
-		return Vector3( x/rhs.x, y/rhs.y, z/rhs.z );
+	Vector3 operator/( float rhs ){
+		return Vector3( x/rhs, y/rhs, z/rhs );
 	}
 	Vector3 operator+=( const Vector3 &rhs ){
-		return Vector3( x+=rhs.x, y+=rhs.y, z+=rhs.z );
+		x+=rhs.x; y+=rhs.y; z+=rhs.z; return *this;
 	}
 	Vector3 operator-=( const Vector3 &rhs ){
-		return Vector3( x-=rhs.x, y-=rhs.y, z-=rhs.z );
+		x-=rhs.x; y-=rhs.y; z-=rhs.z; return *this;
 	}
-	Vector3 operator*=( const Vector3 &rhs ){
-		return Vector3( x*=rhs.x, y*=rhs.y, z*=rhs.z );
+	Vector3 operator*=( float rhs ){
+		x*=rhs; y*=rhs; z*=rhs; return *this;
 	}
-	Vector3 operator/=( const Vector3 &rhs ){
-		return Vector3( x/=rhs.x, y/=rhs.y, z/=rhs.z );
+	Vector3 operator/=( float rhs ){
+		x/=rhs; y/=rhs; z/=rhs; return *this;
 	}
 	
 	float magnitude(){
@@ -63,8 +64,9 @@ struct Vector3{
 		return Vector3( y*rhs.z - z*rhs.y, z*rhs.x - x*rhs.z, x*rhs.y - y*rhs.x );
 	}
 	
-	friend ostream& operator==( ostream &os, const Vector3 &rhs ){
+	friend std::ostream& operator<<( std::ostream &os, const Vector3 &rhs ){
 		os << rhs.x << "," << rhs.y << "," << rhs.z;
 		return os;
 	}
 };
+#endif
